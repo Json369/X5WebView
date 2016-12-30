8aa874108d0be4ae2d2e6d73a47314679cc5e744
996717540@qq.com <xh50575057>
2016-12-30 13:48:47 +0800

    x5WebView

---
+++ app/src/main/java/com/example/xcar/x5webview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.xcar.x5webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView= (WebView) findViewById(R.id.x5WebView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.xcar.com.cn/");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
